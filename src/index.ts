0c630dafb74fad768a7f073aca9848a18ad550ca
haroldCoder <haroldc2005@gmail.com>
2023-10-26 12:04:56 -0500

    save data in redis & get data of redis

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+import express from "express";
+import responseTime from "response-time";
+import {createClient} from "redis";
+import dotenv from "dotenv"
+
+const app = express();
+dotenv.config();
+
+app.use(responseTime());
+
+const client = createClient({
+    url: process.env.URL_REDIS
+})
+
+client.on('error', err => console.log('Redis Client Error', err));
+client.connect();
+
+app.get("/characters", async(req, res)=>{
+    client.get('characters')
+    .then((data)=>{
+        if(!data){
+            console.log([]);
+            res.json([]);
+            return;
+        }
+        res.json(JSON.parse(data));
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.send(err);
+    })
+
+    const response = await axios.get(`https://rickandmortyapi.com/api/character`);
+    client.set('characters', JSON.stringify(response.data)).then(()=>{
+        res.json(response.data);
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+
+app.listen(1000, ()=>{
+    console.log(`Server on port 1000`);
+})
